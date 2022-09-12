8525cee22a11fb324221eab6490521684daa8b6e
menduz <github@menduz.com>
2022-09-12 18:10:30 -0300

    fix build script

--- .site-generator/index.js
+++ .site-generator/index.js
@@ -1,6 +1,7 @@
-module.exports = function({ context }) {
-  context.DRAFT = !!process.env.IS_DRAFT || !!process.env.CF_PAGES_BRANCH
-  if (process.env.CF_PAGES_URL && process.env.CF_PAGES_BRANCH) {
+module.exports = function ({ context }) {
+  const isBranchOrLocal = !!process.env.IS_DRAFT || process.env.CF_PAGES_BRANCH !== "main"
+  context.DRAFT = isBranchOrLocal
+  if (process.env.CF_PAGES_URL && isBranchOrLocal) {
     context.baseUrl = process.env.CF_PAGES_URL
   }
 }
