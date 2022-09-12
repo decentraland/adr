c6c184f940a9310bee804aa2263ca35ec15a3e65
menduz <github@menduz.com>
2022-09-12 18:03:52 -0300

    set env vars

--- .site-generator/index.js
+++ .site-generator/index.js
@@ -1,3 +1,6 @@
 module.exports = function({ context }) {
-  context.DRAFT = !!process.env.IS_DRAFT
+  context.DRAFT = !!process.env.IS_DRAFT || !!process.env.CF_PAGES_BRANCH
+  if (process.env.CF_PAGES_URL) {
+    context.baseUrl = process.env.CF_PAGES_URL
+  }
 }
